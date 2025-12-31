e75a5aa1a107e9a0d80ef8302e8ad21536ec088c
yume0222 <rin_rin_lovebird@icloud.com>
2025-12-31 09:59:58 +0900

    修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://media-copy.vercel.app'),
+  metadataBase: new URL("https://media-copy.vercel.app"),
   title: {
-    template: '%s | 模写修行メディアのコーディング',
-    default: '模写修行メディアのコーディング',
+    template: "%s | 模写修行メディアのコーディング",
+    default: "模写修行メディアのコーディング",
   },
   description:
-    '模写修行メディアのコーディングをNext.js＋ヘッドレスCMSで作成したサイトです。',
+    "模写修行メディアのコーディングをNext.js＋ヘッドレスCMSで作成したサイトです。",
   openGraph: {
-    title: '模写修行メディアのコーディング',
+    title: "模写修行メディアのコーディング",
     description:
-      '模写修行メディアのコーディングをNext.js＋ヘッドレスCMSで作成したサイトです。',
-    images: ['/ogp.png'],
+      "模写修行メディアのコーディングをNext.js＋ヘッドレスCMSで作成したサイトです。",
+    images: ["/ogp.png"],
   },
   alternates: {
-    canonical: 'https://media-copy.vercel.app',
+    canonical: "https://media-copy.vercel.app",
   },
 };
 
@@ -30,8 +30,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <Header />
-        {children}
+        <div className="content">
+          <Header />
+          <main>{children}</main>
+        </div>
         <Footer />
       </body>
     </html>
